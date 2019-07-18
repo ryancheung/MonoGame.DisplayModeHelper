1c5792e7b5e3faea9099c338b4dadb8b9158cd8c
ryancheung <ryancheung.go@gmail.com>
2019-07-18 14:27:41 +0800

    Add winform test project

---
+++ MonoGame.DisplayModeHelper.Test/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MonoGame.DisplayModeHelper;
+
+namespace MonoGame.DisplayModeHelper.Test
+{
+    public partial class Form1 : Form
+    {
+        private int currentScreenWidth = 0, currentScreenHeight = 0;
+
+        public Form1()
+        {
+			currentScreenWidth = Screen.PrimaryScreen.Bounds.Width;
+			currentScreenHeight = Screen.PrimaryScreen.Bounds.Height;
+
+            InitializeComponent();
+
+            FormClosing += Form1_FormClosing;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ResolutionHelper.ChangeResolution(currentScreenWidth, currentScreenHeight);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DEVMODE1 selectedMode;
+            if (DisplayModes.SupportedDisplayModes.TryGetValue(listBox1.SelectedItem.ToString(), out selectedMode))
+            {
+                ResolutionHelper.ChangeResolution(selectedMode.dmPelsWidth, selectedMode.dmPelsHeight);
+            }
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            this.CenterToScreen();
+
+            foreach (var mode in DisplayModes.SupportedDisplayModes)
+            {
+                this.listBox1.Items.Add(mode.Key);
+            }
+		}
+
+		private void button1_Click(object sender, System.EventArgs e)
+		{
+			MessageBox.Show(string.Format("User Resolution is {0}x{1}", currentScreenWidth, currentScreenHeight));
+		}
+
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            ResolutionHelper.ChangeResolution(currentScreenWidth, currentScreenHeight);
+        }
+    }
+}
---
+++ MonoGame.DisplayModeHelper.Test/Form1.designer.cs
@@ -0,0 +1,88 @@
+﻿using System.Windows.Forms;
+
+namespace MonoGame.DisplayModeHelper.Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click Here To Get Resolution";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(40, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(208, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Click Here To Retaine Resolution";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(40, 192);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(523, 292);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 600);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+		private Button button1;
+		private Button button2;
+        private ListBox listBox1;
+    }
+}
+
